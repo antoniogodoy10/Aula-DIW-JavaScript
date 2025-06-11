86e51f954661eebb5a02f3834ce61721ab91bb68
antoniogodoy10 <godoyantonio735@gmail.com>
2025-06-11 20:53:24 -0300

    Teste JavaScript

---
+++ Aula-DIW-JavaScript/script.js
@@ -0,0 +1,6 @@
+var elem = document.getElementById ('btnHello');
+elem.addEventListener ('click', function()
+{
+    var divTela = document.getElementById ("tela");
+    divTela.innerHTML = "Ola, thonyzada";
+}, false);

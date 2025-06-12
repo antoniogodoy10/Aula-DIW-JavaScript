3e0543451d8786eb33c0c7b13cfb12a59c6f1c4e
antoniogodoy10 <godoyantonio735@gmail.com>
2025-06-12 16:38:44 -0300

    Adição dos estados ao clicar o botão Carrega Dados

--- Aula-DIW-JavaScript/app.js
+++ Aula-DIW-JavaScript/app.js
@@ -1,9 +1,9 @@
 let cidades = [
-   {"id": 1, "cidade": "Belo Horizonte", "estado": "MG", "populacao": 3800000},
-   {"id": 2, "cidade": "São Paulo", "estado": "SP", "populacao": 11800000},
-   {"id": 3, "cidade": "Rio de Janeiro", "estado": "RJ", "populacao": 5300000},
-   {"id": 4, "cidade": "Curitiba", "estado": "PR", "populacao": 1200000},
-   {"id": 5, "cidade": "Fortaleza", "estado": "CE", "populacao": 2100000}
+    { "id": 1, "cidade": "Belo Horizonte", "estado": "MG", "populacao": 3800000 },
+    { "id": 2, "cidade": "São Paulo", "estado": "SP", "populacao": 11800000 },
+    { "id": 3, "cidade": "Rio de Janeiro", "estado": "RJ", "populacao": 5300000 },
+    { "id": 4, "cidade": "Curitiba", "estado": "PR", "populacao": 1200000 },
+    { "id": 5, "cidade": "Fortaleza", "estado": "CE", "populacao": 2100000 }
 ]
 
 function mudaFundo() {
@@ -11,7 +11,18 @@ function mudaFundo() {
     document.body.style.backgroundColor = cor
 }
 
-function carregaDados(){
-    let ulCidades = document.getElementById('ulCidades')
-    ulCidades.innerHTML = '<li>Nova Yorque</li>'
+document.getElementById('btnCarregaDados').addEventListener ('click', carregaDados)
+
+document.getElementById('btnMudaFundo').addEventListener ('click', mudaFundo)
+
+function carregaDados() {
+    let ulCidades = document.getElementById('ulCidades');
+    strTextoHTML = '';
+
+    for(let i = 0; i < cidades.length; i++){
+       let cidade = cidades[i];
+       strTextoHTML += `<li>${cidade.cidade} - ${cidade.estado}</li>`
+    }
+
+    ulCidades.innerHTML = strTextoHTML;
 }

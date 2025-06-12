a4d30a97c81fd3a5a7e31ea507b2e4ab5392c1be
antoniogodoy10 <godoyantonio735@gmail.com>
2025-06-12 12:54:04 -0300

    Inserção da variável cidades no JS

--- Aula-DIW-JavaScript/app.js
+++ Aula-DIW-JavaScript/app.js
@@ -1,4 +1,11 @@
+let cidades = {
+       {"id" : 1, "cidade"   "Belo Horizonte", "estado" : "MG", "populacao": 1800000},
+       {"id" : 1, "cidade"   "São Paulo", "estado" : "SP", "populacao": 1800000},
+       {"id" : 1, "cidade"   "Rio de Janeiro", "estado" : "RJ", "populacao": 1800000},
+       {"id" : 1, "cidade"   "Curitiba", "estado" : "PR", "populacao": 1800000},
+       {"id" : 1, "cidade"   "Fortaleza", "estado" : "CE", "populacao": 1800000}
+}
 function mudaFundo() {
-    let cor = document.getElementById ('corfundo').value
-document.body.style.backgroundColor = cor
+    let cor = document.getElementById('corfundo').value
+    document.body.style.backgroundColor = cor
 }
